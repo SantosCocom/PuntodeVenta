ae1801b420543abab99043dda1569a4aaaee9c37
MiguelKantun <41477462+MiguelKantun@users.noreply.github.com>
2018-07-26 10:17:54 -0500

    Agregar clientes
    Agregue el apartado de agregar clíentes

---
+++ agregarClientes/agregarClientes/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MIKE
+ * Date: 13/07/2018
+ * Time: 07:47 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace agregarClientes
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new FormAgregarCliente());
+		}
+		
+	}
+}

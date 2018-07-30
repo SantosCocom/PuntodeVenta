1d7f456a8f9463552deee64264f99341c584a202
SantosCocom <41162711+SantosCocom@users.noreply.github.com>
2018-07-30 10:39:06 -0500

    Menú Principal
    Diseño de entrega para Jornadas

---
+++ MenuPrincipal/MenuPrincipal/FormDetalleVentas.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Santos
+ * Date: 30/07/2018
+ * Time: 07:43 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NovedadesLirio;
+using AccesoADatos;
+namespace MenuPrincipal
+{
+	/// <summary>
+	/// Description of FormDetalleVentas.
+	/// </summary>
+	public partial class FormDetalleVentas : Form
+	{
+		public FormDetalleVentas()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void TxtClaveVentasTextChanged(object sender, EventArgs e)
+		{
+			VentasController VER =new VentasController();
+			VER.buscardetalles(txtClaveVentas,dgvVentas);
+		}
+		void FormDetalleVentasLoad(object sender, EventArgs e)
+		{
+			VentasController VER =new VentasController();
+			VER.buscardetalles(txtClaveVentas,dgvVentas);
+		}
+		void BtnSalirClick(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("¿Está usted seguro que desea salir?", "", MessageBoxButtons.YesNo)== DialogResult.Yes) 
+			{
+			this.Close();
+			}
+		}
+		
+		
+		
+	}
+}
